5235740a73f657a05ec4d772916f1cc51c0c9b5b
Ewelina Singh <2singe84@solent.ac.uk>
2019-05-15 18:51:08 +0000

    Added the downloads folder

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,34 @@
 <template>
   <div> 
     <h1> Home </h1>
+    <h3> PatternLibrary provides component templates to use within your website! </h3>
+    <hr>
+    
+    <h3> How to Download </h3>
+    <p> Click the below button to download the PatternLibrary. </p>
+    <button class="my-button purple" href="../assets/fb.png"> Download PatternLibrary </button>
+    <hr>
+    
+    <h3> How to Set Up </h3>
+    <hr>
+    
+    
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  metaInfo: {
-      title: 'PatternLibrary - Home',
-      meta: [
-        {charset: 'utf-8'}
-      ]
-  },
-  props: {
-    msg: String
+  export default {
+    name: 'Home',
+    metaInfo: {
+        title: 'PatternLibrary - Home',
+        meta: [
+          {charset: 'utf-8'}
+        ]
+    },
+    props: {
+      msg: String
+    }
   }
-}
 </script>
 
 <style scoped>
